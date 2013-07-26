83f24202d375ce9313a70670085b066876dadb6b
Andres Aguirre Dorelo <aguirrea@gmail.com>
2013-07-26 12:31:03 -0300

    some test code

--- simpleTestBioloidSet.py
+++ simpleTestBioloidSet.py
@@ -18,12 +18,13 @@
 
 import vrep
 import time
+from AXAngle import AXAngle
 
 print 'Program started'
 clientID = vrep.simxStart('127.0.0.1',19998,True,True,5000,5)
 if clientID !=-1:
     print 'Connected to remote API server'
-    ##vrep.simxLoadScene(clientID, "/home/andres/Documentos/maestria/modelos_fisicos_robots/nao_genetico.ttt", 2, vrep.simx_opmode_oneshot_wait)
+    vrep.simxLoadScene(clientID, "/home/andres/Documentos/maestria/modelos_fisicos_robots/nao_genetico.ttt", 2, vrep.simx_opmode_oneshot_wait)
     res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
     if res==vrep.simx_error_noerror:
         print 'Number of objects in the scene: ',len(objs)
@@ -31,30 +32,34 @@ if clientID !=-1:
         print 'Remote API function call returned with error code: '
     ##x =vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
     ##vrep.simxSetBooleanParameter(clientID,vrep.sim_boolparam_display_enabled,0,vrep.simx_opmode_oneshot_wait)
-    error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"LShoulderPitch3#", vrep.simx_opmode_oneshot_wait)
-    print error
-    print LSP_Handle
+    #error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"LKneePitch3#", vrep.simx_opmode_oneshot_wait)
+    #error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"LShoulderPitch3#", vrep.simx_opmode_oneshot_wait)
+    error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"LHipPitch3#", vrep.simx_opmode_oneshot_wait)
     #                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    error, FLOOR_Handle=vrep.simxGetObjectHandle(clientID,"DefaultFloor#", vrep.simx_opmode_oneshot_wait)
     #vrep.simxCheckDistance(clientID, LSP_Handle, FLOOR_Handle, 50,  vrep.simx_opmode_oneshot_wait)
     #colHandle = vrep.simxGetCollisionHandle(clientID, "DefaultFloor#", vrep.simx_opmode_oneshot_wait)
     #error, collisionState = vrep.simxReadCollision(clientID, colHandle, vrep.simx_opmode_oneshot_wait)
     #print error
     #print collisionState
-    error, position = vrep.simxGetObjectPosition(clientID, LSP_Handle, 600, vrep.simx_opmode_streaming)
-    print error
-    print position
-    
+    angle1 = AXAngle(1023)
+    angle2 = AXAngle(512)
+    error, position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
+    error, position = vrep.simxGetJointPosition(clientID,LSP_Handle, vrep.simx_opmode_streaming)
+    print "al leer posicion error " + str(error) + " posicion " + str(position)
+
     i=0
     while(1):
         if (i%2 == 0):
-            vrep.simxSetJointPosition(clientID,LSP_Handle, -1 , vrep.simx_opmode_streaming)
+            #vrep.simxSetJointPosition(clientID,LSP_Handle, angle1.toVrep() , vrep.simx_opmode_streaming)
+            vrep.simxSetJointPosition(clientID,LSP_Handle, 0, vrep.simx_opmode_streaming)
         else:
-            vrep.simxSetJointPosition(clientID,LSP_Handle, 2, vrep.simx_opmode_streaming)
+            vrep.simxSetJointPosition(clientID,LSP_Handle, 1, vrep.simx_opmode_streaming)
         time.sleep(3)
         print "frame: " + str(i)
         error, position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
-        print error
-        print position
+        error, position = vrep.simxGetJointPosition(clientID,LSP_Handle, vrep.simx_opmode_streaming)
+        print "al leer posicion error " + str(error) + " posicion " + str(position)
+
         #error, collisionState = vrep.simxReadCollision(clientID, FLOOR_Handle, vrep.simx_opmode_oneshot_wait)
         #print "Error: " + str(error)
         #print "State: " + str(collisionState)
