94fc4ce6d9e6b75162abf060500e9d11c40a7bd5
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2014-07-16 16:51:22 -0300

    putting all the simulator stuff in a library

--- Simulator.py
+++ Simulator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Andrés Aguirre Dorelo
 # MINA/INCO/UDELAR
-# helper functions to access to the simulator
+# Helper functions to access simulator services
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,61 +21,78 @@ import vrep
 import time
 import math
 
+from LoadPoses import LoadPoses
+
 FALL_THRESHOLD = 0.07
+localhost='127.0.0.1'
+standadRemoteApiPort=19997
 
-#this data structure is like a cache for the joint handles
-jointHandleMapping = {}
 
-def connectVREP(ipAddr=localhost, port=standadRemoteApiPort):
-    return vrep.simxStart(ipAddr,port,True,True,5000,5)
+class Simulator:
+    def __init__(self):
+        #this data structure is like a cache for the joint handles
+        self.jointHandleMapping = {}
 
-def loadscn(clientID, model=genetic_bioloid):
-    error=vrep.simxLoadScene(clientID, model, 2, vrep.simx_opmode_oneshot_wait)
-    return error
+    def connectVREP(self, ipAddr=localhost, port=standadRemoteApiPort):
+        vrep.simxFinish(-1) # just in case, close all opened connections
+        lp = LoadPoses()
+        pose=lp.getFramePose(1)
+        for joint in pose.keys():
+            self.jointHandleMapping[joint]=0
+        #print joint
+        return vrep.simxStart(ipAddr,port,True,True,5000,5)
 
-def printJointPositions(clientID):
-    error, handles, intData, floatData, stringData=vrep.simxGetObjectGroupData(clientID,vrep.sim_appobj_object_type,0,vrep.simx_opmode_oneshot_wait)
-    itemHandle=0
-    print stringData
-    for name in stringData:
-        error, position = vrep.simxGetJointPosition(clientID,handles[itemHandle], vrep.simx_opmode_streaming)
-        itemHandle=itemHandle+1
-        print name + ":" + str(position)
+    def loadscn(self, clientID, model):
+        error=vrep.simxLoadScene(clientID, model, 2, vrep.simx_opmode_oneshot_wait)
+        return error
 
-def isRobotUp(clientID):
-    error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
-    error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
-    #in case of problems consulting the robot position, consider that the robot is up
-    if error:
-        return error, True
-    else:
-        return error, bioloid_position[2]>FALL_THRESHOLD
+    def printJointPositions(self, clientID):
+        error, handles, intData, floatData, stringData=vrep.simxGetObjectGroupData(clientID,vrep.sim_appobj_object_type,0,vrep.simx_opmode_oneshot_wait)
+        itemHandle=0
+        print stringData
+        for name in stringData:
+            error, position = vrep.simxGetJointPosition(clientID,handles[itemHandle], vrep.simx_opmode_streaming)
+            itemHandle=itemHandle+1
+            print name + ":" + str(position)
 
-def startSim(clientID, screen=True):
-    error=vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
-    if not screen:
-        vrep.simxSetBooleanParameter(clientID,vrep.sim_boolparam_display_enabled,0,vrep.simx_opmode_oneshot_wait)
-    return error
+    def isRobotUp(self, clientID):
+        error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
+        error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
+        #in case of problems consulting the robot position, consider that the robot is up
+        if error:
+            return True
+        else:
+            return bioloid_position[2]>FALL_THRESHOLD
 
-def setJointPosition(clientID, joint, angle):
-    if (jointHandleMapping[joint] > 0):
-        jhandle=jointHandleMapping[joint]
-    else:
-        error, jhandle=vrep.simxGetObjectHandle(clientID,joint,vrep.simx_opmode_oneshot_wait)
-        jointHandleMapping[joint]=jhandle
-    vrep.simxSetJointPosition(clientID,jhandle,angle,vrep.simx_opmode_oneshot_wait)
+    def startSim(self, clientID, screen=True):
+        error=vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+        if not screen:
+            vrep.simxSetBooleanParameter(clientID,vrep.sim_boolparam_display_enabled,0,vrep.simx_opmode_oneshot_wait)
+        return error
+
+    def setJointPosition(self, clientID, joint, angle):
+        if (self.jointHandleMapping[joint] > 0):
+            jhandle=self.jointHandleMapping[joint]
+        else:
+            error, jhandle=vrep.simxGetObjectHandle(clientID,joint,vrep.simx_opmode_oneshot_wait)
+            self.jointHandleMapping[joint]=jhandle
+        vrep.simxSetJointPosition(clientID,jhandle,angle,vrep.simx_opmode_oneshot_wait)
+            
+    def finishSimulation(self, clientID):
+        errorStop=vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+        errorClose=vrep.simxCloseScene(clientID,vrep.simx_opmode_oneshot_wait)
+        error=errorStop or errorClose
+        errorFinish=vrep.simxFinish(clientID)
+        error=error or errorFinish
+        return error
         
-def finishSimulation(clientID):
-    errorStop=vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait)
-    errorClose=vrep.simxCloseScene(clientID,vrep.simx_opmode_oneshot_wait)
-    error=errorStop or errorClose
-    errorFinish=vrep.simxFinish(clientID)
-    error=error or errorFinish
-    return error
-    
-def getBioloidPlannarPosition(clientID):
-    error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
-    error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
-    return bioloid_position[0], bioloid_position[1]
+    def getBioloidPlannarPosition(self, clientID):
+        error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
+        error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
+        return bioloid_position[0], bioloid_position[1]
 
+
+
+sim = Simulator()
+sim.connectVREP()
 
