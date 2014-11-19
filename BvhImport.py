f2035b2cd366d5b135bba7fe5dee3c99d466e994
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2014-11-19 16:24:49 -0200

    new method for getting all the node positions for all the frames using the node name as the identifier

--- BvhImport.py
+++ BvhImport.py
@@ -25,9 +25,15 @@ from BVHToolkit import Animation, Pose, Bone
 
 class BvhImport:
     def __init__(self, file):
-        #Load BVH and get node positions
         bvh_serial_path = os.path.join(os.path.dirname(__file__), file)
         self.animation = Animation.from_bvh(bvh_serial_path)
+        self.nodeNameIndexMapping = {}
+        pose = self.animation.get_pose(1)
+        for j in range(len(pose.positions)):
+            self.nodeNameIndexMapping[pose.bone.node_list[j].name] = j
+
+    def test(self):
+        print self.nodeNameIndexMapping
 
     def listAll(self):
         for i in range(len(self.animation.frames)):
@@ -35,20 +41,32 @@ class BvhImport:
             for j in range(len(pose.positions)):
                 print str(pose.bone.node_list[j].name) + ": " + str(pose.get_position(j))
 
-    def getNodePositions(self,node):
+    def getNodePositions(self,nodeIndex):
         result_x={}
         result_y={}
         result_z={}
         for i in range(len(self.animation.frames)):
             pose = self.animation.get_pose(i)
-            result_x[i]=pose.get_position(node)[0]
-            result_y[i]=pose.get_position(node)[1]
-            result_z[i]=pose.get_position(node)[2]
+            result_x[i]=pose.get_position(nodeIndex)[0]
+            result_y[i]=pose.get_position(nodeIndex)[1]
+            result_z[i]=pose.get_position(nodeIndex)[2]
         return result_x, result_y, result_z
 
+    def getNodePositionsFromName(self,nodeName):
+        result_x={}
+        result_y={}
+        result_z={}
+        nodeIndex=self.nodeNameIndexMapping[nodeName]
+        for i in range(len(self.animation.frames)):
+            pose = self.animation.get_pose(i)
+            result_x[i]=pose.get_position(nodeIndex)[0]
+            result_y[i]=pose.get_position(nodeIndex)[1]
+            result_z[i]=pose.get_position(nodeIndex)[2]
+        return result_x, result_y, result_z
 
-parser = BvhImport("Example1.bvh")
-parser.listAll()
-u, v, z = parser.getNodePositions(0)
+#parser = BvhImport("Example1.bvh")
+#u, v, z = parser.getNodePositionsFromName("hip")
+#print v
+#u, v, z = parser.getNodePositions(0)
+#print v
 
-print v
