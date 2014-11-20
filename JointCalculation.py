951769830d48b07db8ec9769e763bbe9409225e0
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2014-11-20 20:08:22 -0200

    fixing complex calculus for joint calculation

--- JointCalculation.py
+++ JointCalculation.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # Andrés Aguirre Dorelo
+# Damián Ferraro
+# Patricia Polero
+#
 # MINA/INCO/UDELAR
 # 
 # Joint value calculations for the bioloid model from mocap model
@@ -21,43 +24,51 @@
 from BvhImport import BvhImport
 from numpy import array
 from numpy import conjugate
+from numpy import angle
 import math
 
 class JointCalculation:
     def __init__(self, file):
     	self.parser = BvhImport(file)
 
-    def arg(self,z):
-        return math.atan2(z.imag, z.real)
-
     def angle(self,v):
         for i in range(len(v)):
             if v[i].imag >=0:
-    		    v[i]=self.arg(v[i])
+    		    v[i]=angle(v[i])
             else:
-		        v[i]=math.pi+self.arg(-v[i])
+		        v[i]=(math.pi)-angle(-v[i])
         return v
-
+    #from joint3 to joint2 with axis in joint1 anti clockwise
     def calculate(self, joint1, joint2, joint3):
-		x1, y1, z1 = self.parser.getNodePositionsFromName(joint1)
-		x2, y2, z2 = self.parser.getNodePositionsFromName(joint2)
-		x3, y3, z3 = self.parser.getNodePositionsFromName(joint3)
-		ax1 = array(x1.values())
-		ax2 = array(x2.values())
-		ax3 = array(x3.values())
-		ay1 = array(y1.values())
-		ay2 = array(y2.values())
-		ay3 = array(y3.values())
-		u = ax2 - ax1 + 1j*(ay2 - ay1)
-		v = ax3 - ax1 + 1j*(ay3 - ay1)
-		r = self.angle(u*conjugate(v))
-
-jc = JointCalculation("Example1.bvh")
+        x1, y1, z1 = self.parser.getNodePositionsFromName(joint1)
+        x2, y2, z2 = self.parser.getNodePositionsFromName(joint2)
+        x3, y3, z3 = self.parser.getNodePositionsFromName(joint3)
+        ax1 = array(x1.values())
+        ax2 = array(x2.values())
+        ax3 = array(x3.values())
+        ay1 = array(y1.values())
+        ay2 = array(y2.values())
+        ay3 = array(y3.values())
+        u = ax2 - ax1 + 1j*(ay2 - ay1)
+        v = ax3 - ax1 + 1j*(ay3 - ay1)
+        conj=conjugate(v)
+        r = self.angle(u*conjugate(v))*(180/math.pi)
+        return r.real
+        
+jc = JointCalculation("02_02.bvh")
 rElbowYaw = jc.calculate("rForeArm", "rShldr", "rHand")
-rShoulderYaw = jc.calculate("rCollar","chest", "rShldr")
-rShoulderPitch = jc.calculate("rShldr", "rForeArm", "hip")
-rHipYaw = jc.calculate("rFoot", "End Site", "rThigh")
-rHipRoll = jc.calculate("rThigh", "rShin", "rCollar")
-rHipPitch = jc.calculate("rThigh", "rCollar", "rShin")
-rKneePitch = jc.calculate("rShin", "rThigh", "rFoot")
-rAnkle = jc.calculate("rFoot", "rShin", "End Site")
+print rElbowYaw
+print rElbowYaw.min()
+print rElbowYaw.max()
+
+#print "********************************************************************************************"
+#rShoulderYaw = jc.calculate("rCollar","chest", "rShldr")
+#rShoulderPitch = jc.calculate("rShldr", "rForeArm", "hip")
+#rHipYaw = jc.calculate("rFoot", "End Site", "rThigh")
+#rHipRoll = jc.calculate("rThigh", "rShin", "rCollar")
+#rHipPitch = jc.calculate("rThigh", "rCollar", "rShin")
+rKneePitch = jc.calculate("lShin", "lThigh","lFoot")
+#rAnkle = jc.calculate("rFoot", "rShin", "End Site")
+print rKneePitch
+print rKneePitch.min()
+print rKneePitch.max()
