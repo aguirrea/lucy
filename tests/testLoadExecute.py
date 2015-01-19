f6d347f85bd46a8720452a358ef697ae5cee46d6
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-01-19 14:38:17 -0200

    fixing the angle representation and some testing of the joints calculations in the vrep model

--- tests/testLoadExecute.py
+++ tests/testLoadExecute.py
@@ -18,7 +18,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from simulator.Simulator import Simulator
 from simulator.SimLucy   import SimLucy
 from simulator.AXAngle   import AXAngle
 from parser.LoadPoses    import LoadPoses
@@ -32,46 +31,48 @@ angle = AXAngle()
 angleFix = AXAngle()
 angleExecute = AXAngle()
 
-mocapFile = os.getcwd()+"/cmu_mocap/xml/test.xml"
+mocapFile = os.getcwd()+"/../cmu_mocap/xml/test2.xml"
 lp = LoadPoses(mocapFile)
-sim = Simulator()
 lucy = SimLucy(True)
 
 poseExecute={}
 poseFix={}
 
 poseFix["R_Shoulder_Yaw"] = 0
-poseFix["R_Shoulder_Pitch"] = -90
+poseFix["R_Shoulder_Pitch"] = -360
 poseFix["R_Hip_Yaw"] = 0
 poseFix["R_Hip_Roll"] = 0
-poseFix["R_Hip_Pitch"] = 20
-poseFix["R_Knee"] = 10
-poseFix["R_Ankle_Pitch"] = -5
+poseFix["R_Hip_Pitch"] = -60
+poseFix["R_Knee"] = 20 
+poseFix["R_Ankle_Pitch"] = 0
 poseFix["R_Elbow_Yaw"] = 0
 poseFix["R_Ankle_Roll"] = 0
 
 poseFix["L_Shoulder_Yaw"] = 0
-poseFix["L_Shoulder_Pitch"] = -90
+poseFix["L_Shoulder_Pitch"] = -360
 poseFix["L_Hip_Yaw"] = 0
 poseFix["L_Hip_Roll"] = 0
-poseFix["L_Hip_Pitch"] = 20
-poseFix["L_Knee"] = 10
-poseFix["L_Ankle_Pitch"] = -5
+poseFix["L_Hip_Pitch"] = -60
+poseFix["L_Knee"] = 20
+poseFix["L_Ankle_Pitch"] = 0
 poseFix["L_Elbow_Yaw"] = 0
 poseFix["L_Ankle_Roll"] = 0
 
 avoid_joints = ["R_Hip_Yaw", "R_Shoulder_Yaw", "L_Hip_Yaw", "R_Shoulder_Yaw", "L_Hip_Roll", "R_Hip_Roll", "L_Ankle_Roll", "R_Ankle_Roll", "L_Ankle_Pitch", "R_Ankle_Pitch"] 
+#avoid_joints = [] 
 frameQty=lp.getFrameQty()
 while  True:
     for index in range(frameQty):
         pose=lp.getFramePose(index)
         for joint in pose.keys():
                 if joint not in avoid_joints:
-                    #print joint, pose[joint]
-                    angleExecute.setValue(630 - pose[joint] + poseFix[joint])
-                    poseExecute[joint] = angleExecute.toVrep() 
+                    #print joint, pose[joint], pose[joint] + poseFix[joint] 
+                    angleExecute.setDegreeValue(pose[joint]+ poseFix[joint])
+                    #angleExecute.setValue(630 - pose[joint] + poseFix[joint])
+                    #poseExecute[joint] = abs(angleExecute.toVrep())
+                    poseExecute[joint] = angleExecute.toVrep()
                     #print joint, poseExecute[joint], angleExecute.getValue()  
-        #print poseExecute['L_Knee']     
+        print poseExecute['L_Shoulder_Pitch'], pose['L_Shoulder_Pitch']     
         lucy.executeFrame(poseExecute)
 lucy.stopLucy()  
 print 'Program ended'
