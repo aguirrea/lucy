218979877436c5443ca4ab28960bf2aad4241606
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-01-29 21:00:22 -0200

    genetic representation for learning motions

---
+++ DTIndividualProperty.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Andrés Aguirre Dorelo
+# MINA/INCO/UDELAR
+#
+# Datatype for the individual property
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+class DTIndividualProperty(object):
+    def __init__(self):
+        self.poseFix={}
+        self.avoidJoints=[]
+
+    def avoidJoint(self, joint):
+        return joint in self.avoidJoints
+
+    def getPoseFix(self, joint)
+        if joint in poseFix.keys():
+            res = poseFix[joint]
+        else:
+            res = 0
+        return res
+
+class DTIndividualPropertyCMUDaz(DTIndividualProperty):
+
+    def __init__(self):
+        self.avoidJoints = ["R_Hip_Yaw", "R_Shoulder_Yaw", "L_Hip_Yaw", "R_Shoulder_Yaw", "L_Hip_Roll", "R_Hip_Roll", "L_Ankle_Roll", "R_Ankle_Roll"] 
+        
+        self.poseFix["R_Shoulder_Yaw"] = 0
+        self.poseFix["R_Shoulder_Pitch"] = -45
+        self.poseFix["R_Hip_Yaw"] = 0
+        self.poseFix["R_Hip_Roll"] = 0
+        self.poseFix["R_Hip_Pitch"] = -50
+        self.poseFix["R_Knee"] = 0 
+        self.poseFix["R_Ankle_Pitch"] = 9
+        self.poseFix["R_Elbow_Yaw"] = 0
+        self.poseFix["R_Ankle_Roll"] = 0
+
+        self.poseFix["L_Shoulder_Yaw"] = 0
+        self.poseFix["L_Shoulder_Pitch"] = -50
+        self.poseFix["L_Hip_Yaw"] = 0
+        self.poseFix["L_Hip_Roll"] = 0
+        self.poseFix["L_Hip_Pitch"] = -50
+        self.poseFix["L_Knee"] = 0
+        self.poseFix["L_Ankle_Pitch"] = 9
+        self.poseFix["L_Elbow_Yaw"] = 0
+        self.poseFix["L_Ankle_Roll"] = 0
+
---
+++ Individual.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Andrés Aguirre Dorelo
+# MINA/INCO/UDELAR
+#
+# Datatype for the individual property
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from simulator.SimLucy    import SimLucy
+from simulator.AXAngle    import AXAngle
+from parser.LoadPoses     import LoadPoses
+from DTIndividualProperty import DTIndividualProperty
+
+
+class Individual:
+
+    def __init__(self, file):
+        self.property = DTIndividualPropertyCMUDaz() #TODO review python herence
+        self.fitness = 0
+        self.mocapFile = file
+        self.lp = LoadPoses(self.mocapFile)
+        self.lucy = SimLucy(False)
+
+    def execute(self):
+        angleExecute = AXAngle()
+        lucyIsDown = False
+        while (self.lucy.isLucyUp() and i <= self.lp.getFrameQty()):
+            poses = self.lp.getPoses(i)
+            i = i + 1
+            for joint in poses.keys():
+                if !property.avoidJoint(joint):
+                    angleExecute.setDegreeValue(poses[joint] + property.getPoseFix(joint))
+                    pose[joint].toVrep()
+            self.lucy.executeFrame(pose)
+        self.fitness = self.lucy.getFitness()        
+
+    def getPoseQty(self):
+        return self.lp.getFrameQty()
+
+    def getMostSimilarPose(self, pose):
+        pass
+
