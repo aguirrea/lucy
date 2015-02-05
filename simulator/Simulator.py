8998b8f1982599c52bf274df722e284f91bf480c
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-02-04 23:58:52 -0200

    adding error management and review of the vrep op_mode

--- simulator/Simulator.py
+++ simulator/Simulator.py
@@ -30,12 +30,22 @@ standadRemoteApiPort=19997
 
 class Simulator:
     def __init__(self):
+        self.firstCallsimxGetJointPosition = True
+        self.getObjectPositionFirstTime = True
         #this data structure is like a cache for the joint handles
         self.jointHandleMapping = {} 
         robotConf = LoadRobotConfiguration()
         self.LucyJoints = robotConf.getJointsName()            
         for joint in self.LucyJoints:
             self.jointHandleMapping[joint]=0
+
+    def getObjectPositionWrapper(self, clientID, LSP_Handle):
+            if self.getObjectPositionFirstTime:
+                error, ret = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
+            else:
+                self.getObjectPositionFirstTime = False
+                error, ret = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_buffer) 
+            return error, ret
 
     def connectVREP(self, ipAddr=localhost, port=standadRemoteApiPort):
         vrep.simxFinish(-1) # just in case, close all opened connections
@@ -46,25 +56,22 @@ class Simulator:
         return error
 
     def printJointPositions(self, clientID):
-        error, handles, intData, floatData, stringData=vrep.simxGetObjectGroupData(clientID,vrep.sim_appobj_object_type,0,vrep.simx_opmode_oneshot_wait)
+        error, handlers, intData, floatData, stringData=vrep.simxGetObjectGroupData(clientID,vrep.sim_appobj_object_type,0,vrep.simx_opmode_oneshot_wait)
         itemHandle=0
         print stringData
         for name in stringData:
-            error, position = vrep.simxGetJointPosition(clientID,handles[itemHandle], vrep.simx_opmode_streaming)
+            error, position = vrep.simxGetJointPosition(clientID,handlers[itemHandle], vrep.simx_opmode_streaming)
             itemHandle=itemHandle+1
             print name + ":" + str(position)
 
     def isRobotUp(self, clientID):
-        error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
-        error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
-        #in case of problems consulting the robot position, consider that the robot is up
-        if error:
-            return True
-        else:
-            return bioloid_position[2]>FALL_THRESHOLD
+        error = False
+        error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait) or error
+        error, bioloid_position = self.getObjectPositionWrapper(clientID, LSP_Handle) or error 
+        return error, bioloid_position[2]>FALL_THRESHOLD
 
     def startSim(self, clientID, screen=True):
-        error=vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot_wait)
+        error=vrep.simxStartSimulation(clientID,vrep.simx_opmode_oneshot)
         if not screen:
             vrep.simxSetBooleanParameter(clientID,vrep.sim_boolparam_display_enabled,0,vrep.simx_opmode_oneshot_wait)
         return error
@@ -96,7 +103,23 @@ class Simulator:
             error, jhandle=vrep.simxGetObjectHandle(clientID,joint,vrep.simx_opmode_oneshot)
             self.jointHandleMapping[joint]=jhandle
         vrep.simxSetJointPosition(clientID,jhandle,angle,vrep.simx_opmode_oneshot)
-            
+
+    def getJointPositionNonBlock(self, clientID, joint):
+
+        if (self.jointHandleMapping[joint] > 0):
+            jhandle=self.jointHandleMapping[joint]
+        else:
+            error, jhandle=vrep.simxGetObjectHandle(clientID,joint,vrep.simx_opmode_oneshot)
+            self.jointHandleMapping[joint]=jhandle
+        if self.firstCallsimxGetJointPosition:
+            self.firstCallsimxGetJointPosition = False
+            error, value = vrep.simxGetJointPosition(clientID,jhandle,vrep.simx_opmode_streaming)
+        else:
+            error, value = vrep.simxGetJointPosition(clientID,jhandle,vrep.simx_opmode_buffer)
+        if error:
+            print "error: ", error
+        return error, value
+
     def finishSimulation(self, clientID):
         errorStop=vrep.simxStopSimulation(clientID,vrep.simx_opmode_oneshot_wait)
         errorClose=vrep.simxCloseScene(clientID,vrep.simx_opmode_oneshot_wait)
@@ -106,9 +129,12 @@ class Simulator:
         return error
         
     def getBioloidPlannarPosition(self, clientID):
-        error, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
-        error, bioloid_position = vrep.simxGetObjectPosition(clientID, LSP_Handle, -1, vrep.simx_opmode_streaming)
-        return bioloid_position[0], bioloid_position[1]
+        errorHandler, LSP_Handle=vrep.simxGetObjectHandle(clientID,"Bioloid", vrep.simx_opmode_oneshot_wait)
+        errorObjectPosition, bioloid_position = self.getObjectPositionWrapper(clientID, LSP_Handle) 
+        return errorHandler or errorObjectPosition, bioloid_position[0], bioloid_position[1]
+
+
+
 
 
 
