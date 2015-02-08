90185d58d505bb0d0abc9c7dd0d156ff27fc8c51
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-02-08 18:52:52 -0200

    adding exceptions management

---
+++ errors/VrepException.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Andrés Aguirre Dorelo
+# MINA/INCO/UDELAR
+# 
+# Error handling, vrep errors encapsulation and management
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+class VrepException(Exception):
+    def __init__(self, message, errors):
+        super(VrepException, self).__init__(message)
+        self.errors = errors
+
+
+
