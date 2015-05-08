f352a3551df0433530b22a604fdd2382152126e0
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-05-08 17:04:23 -0300

    using the data type for the representation of the genetic material and the property information to adjust the joints and abstracting into a vainilla representation.

--- Individual.py
+++ Individual.py
@@ -3,7 +3,7 @@
 # Andrés Aguirre Dorelo
 # MINA/INCO/UDELAR
 #
-# Datatype for the individual property
+# Genetic Algorithm individual representation
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,35 +18,31 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from simulator.SimLucy    import SimLucy
-from simulator.AXAngle    import AXAngle
-from parser.LoadPoses     import LoadPoses
-from DTIndividualProperty import DTIndividualProperty, DTIndividualPropertyCMUDaz, DTIndividualPropertyVanilla, DTIndividualPropertyBaliero
-from Pose                 import Pose
-from LoadSystemConfiguration import LoadSystemConfiguration
+from simulator.SimLucy                import SimLucy
+from simulator.AXAngle                import AXAngle
+from parser.LoadPoses                 import LoadPoses
+from DTIndividualProperty             import DTIndividualProperty, DTIndividualPropertyCMUDaz, DTIndividualPropertyVanilla, DTIndividualPropertyBaliero
+from DTIndividualGeneticMaterial      import DTIndividualGeneticMaterial, DTIndividualGeneticTimeSerieFile, DTIndividualGeneticMatrix
+from Pose                             import Pose
+from LoadSystemConfiguration          import LoadSystemConfiguration
 from simulator.LoadRobotConfiguration import LoadRobotConfiguration
 
 import os #only for the tests
 import glob #only for the tests
 import time
+import xml.etree.cElementTree as ET
 
 class Individual:
 
-    def __init__(self, idividualProperty, file = None, geneMatrix = None):
+    def __init__(self, idividualProperty, individualGeneticMaterial):
         self.property = idividualProperty
         self.fitness = 0
-        self.mocapFile = file
+
         self.robotConfig = LoadRobotConfiguration()
+        self.configuration = LoadSystemConfiguration()
         
-        #if the constructor receive a genetic matrix instead a file to create the individual
-        if self.mocapFile == None and not(geneMatrix == None):
-            self.genomeMatrix = geneMatrix
-            self.poseSize = len(geneMatrix)
-        #if the constructor receive a file intead a genetic matrix to create the individual
-        elif not self.mocapFile == None:
-            self.lp = LoadPoses(self.mocapFile)
-            self.poseSize = self.lp.getFrameQty()
-            self.genomeMatrix = [[self.lp.getPose(i).getValue(j) for j in self.robotConfig.getJointsName()] for i in range(self.poseSize)] 
+        self.genomeMatrix = individualGeneticMaterial.getGeneticMatrix()
+        self.poseSize = len(self.genomeMatrix)
 
         self.lucy = SimLucy(True)
 
@@ -56,6 +52,18 @@ class Individual:
             self.genomeMatrixJointNameIDMapping[jointName]=i
             i=i+1
 
+        dontSupportedJoints = self.configuration.getVrepNotImplementedBioloidJoints()
+        self.robotImplementedJoints = []
+        robotJoints = self.robotConfig.getJointsName()
+        for joint in robotJoints:
+            if joint not in dontSupportedJoints:
+                self.robotImplementedJoints.append(joint)
+
+        for i in range(self.poseSize):
+            for joint in self.robotImplementedJoints:
+                value = self.genomeMatrix[i][self.genomeMatrixJointNameIDMapping[joint]] + self.property.getPoseFix(joint)
+                self.genomeMatrix[i][self.genomeMatrixJointNameIDMapping[joint]]=value
+
     def execute(self):
         angleExecute = AXAngle()
         poseExecute={}
@@ -63,7 +71,7 @@ class Individual:
         while (self.lucy.isLucyUp() and i < self.poseSize):
             for joint in self.robotConfig.getJointsName():
                 if not(self.property.avoidJoint(joint)):
-                    value = self.genomeMatrix[i][self.genomeMatrixJointNameIDMapping[joint]] + self.property.getPoseFix(joint)
+                    value = self.genomeMatrix[i][self.genomeMatrixJointNameIDMapping[joint]]
                     angleExecute.setDegreeValue(value)
                     poseExecute[joint] = angleExecute.toVrep() 
             i = i + 1  
@@ -97,17 +105,31 @@ class Individual:
         self.genomeMatrix = geneMatrix
         self.poseSize = len(geneMatrix)
 
-prop = DTIndividualPropertyCMUDaz()
-propVanilla = DTIndividualPropertyVanilla()
-balieroProp = DTIndividualPropertyBaliero()
+    def persist(self,file):
+        root = ET.Element("root")
+        lucy = ET.SubElement(root, "Lucy")
+        for frameIt in range(self.poseSize):
+            frame = ET.SubElement(lucy, "frame")
+            frame.set("number" , str(frameIt))
+            for joint in self.robotImplementedJoints:
+                xmlJoint = ET.SubElement(frame, joint)
+                joint_id = self.robotConfig.loadJointId(joint)
+                pos = self.genomeMatrix[frameIt][self.genomeMatrixJointNameIDMapping[joint]]
+                xmlJointAngle = xmlJoint.set("angle" , str(pos))
+        tree = ET.ElementTree(root)
+        tree.write(file)
 
-conf = LoadSystemConfiguration()
+#prop = DTIndividualPropertyCMUDaz()
+#propVanilla = DTIndividualPropertyVanilla()
+#balieroProp = DTIndividualPropertyBaliero()
 
-CMUxmlDir = os.getcwd()+conf.getDirectory("Transformed CMU mocap Files")
-GAwalkDir = os.getcwd()+conf.getDirectory("GAWalk Files")
-UIBLHDir = os.getcwd()+conf.getDirectory("UIBLH mocap Files")
-BalieroDir = os.getcwd()+conf.getDirectory("Baliero transformed walk Files")
-ADHOCDir = os.getcwd()+conf.getDirectory("ADHOC Files")
+#conf = LoadSystemConfiguration()
+
+#CMUxmlDir = os.getcwd()+conf.getDirectory("Transformed CMU mocap Files")
+#GAwalkDir = os.getcwd()+conf.getDirectory("GAWalk Files")
+#UIBLHDir = os.getcwd()+conf.getDirectory("UIBLH mocap Files")
+#BalieroDir = os.getcwd()+conf.getDirectory("Baliero transformed walk Files")
+#ADHOCDir = os.getcwd()+conf.getDirectory("ADHOC Files")
 
 #for filename in glob.glob(os.path.join(GAwalkDir, '*.xml')):
 #    print 'executing individual: ' + filename
@@ -129,10 +151,11 @@ ADHOCDir = os.getcwd()+conf.getDirectory("ADHOC Files")
 #    walk = Individual(filename, propVanilla)
 #    walk.execute()
 
-for filename in glob.glob(os.path.join(CMUxmlDir, '*.xml')):
-    print 'executing individual: ' + filename
-    walk = Individual(prop, filename)
-    walk.execute()
+#for filename in glob.glob(os.path.join(CMUxmlDir, '*.xml')):
+#    print 'executing individual: ' + filename
+#    walk = Individual(prop, DTIndividualGeneticTimeSerieFile(filename))
+#    walk.execute()
+#    walk.persist("borrame.xml")
 
 #for filename in glob.glob(os.path.join(ADHOCDir, '*.xml')):
 #    print 'executing individual: ' + filename
