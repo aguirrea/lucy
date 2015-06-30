de53b999daf6a8e96fb6a6a22a3e16b893cd6746
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-06-30 17:47:12 -0300

    implementing new crossover operator

--- crossovers.py
+++ crossovers.py
@@ -20,7 +20,15 @@
 
 import pyevolve.Util as Util
 from random import randint as rand_randint, choice as rand_choice
-#todo implement the distance function
+import math
+
+
+def diff(frame1, frame2):
+   diff=0
+   for position in range(len(frame1)):
+      diff = diff + math.fabs(frame1[position]-frame2[position])
+   return diff
+
 def G2DListCrossoverSingleHPoint(genome, **args):
    """ The crossover of G2DList, Single Horizontal Point """
  
@@ -42,5 +50,49 @@ def G2DListCrossoverSingleHPoint(genome, **args):
       brother.resetStats()
       for i in xrange(cut, brother.getHeight()):
          brother[i][:] = gMom[i][:]
+
+   return (sister, brother)
+
+def G2DListCrossoverSingleNearHPoint(genome, **args):
+   """ The crossover of G2DList, Single Horizontal Point """
+ 
+   sister = None
+   brother = None
+   gMom = args["mom"]
+   gDad = args["dad"]
+   minimalDiff = 1000000
+   minimalDiffPosition = 0
+
+   cut = rand_randint(1, gMom.getHeight() - 1)
+   
+   frame1 = gMom[cut]
+
+   for position in range(gDad.getHeight()):
+      frameDiff = diff(frame1, gDad[position])
+      if frameDiff < minimalDiff and frameDiff>5: #avoid 
+         minimalDiff = frameDiff
+         minimalDiffPosition = position
+         print minimalDiffPosition, minimalDiff, gDad.getHeight()
+   print "difference between poses: ", minimalDiff
+
+   frame2 = gDad[minimalDiffPosition]
+
+   print "frame1", frame1
+
+   print "frame2", frame2
+
+   sister = gMom.clone()
+   brother = gDad.clone()
+   if args["count"] >= 1:
+      sister.resetStats()
+      for i in xrange(minimalDiffPosition, brother.getHeight()):
+         if cut+(i-minimalDiffPosition) < sister.getHeight():
+            sister[cut+(i-minimalDiffPosition)][:] = gDad[i][:]
+
+   if args["count"] == 2:
+      brother.resetStats()
+      for i in xrange(minimalDiffPosition, sister.getHeight()):
+         if i < brother.getHeight() and cut+(i-minimalDiffPosition) < gMom.getHeight():
+            brother[i][:] = gMom[cut+(i-minimalDiffPosition)][:]
 
    return (sister, brother)
