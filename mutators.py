c0a20a2bf64113194a7810532e935572feb41aa2
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-12-10 11:28:47 -0300

    code beautify and parameterization

--- mutators.py
+++ mutators.py
@@ -3,7 +3,8 @@
 # Andrés Aguirre Dorelo
 # MINA/INCO/UDELAR
 #
-# Lucy mutator operator based on pyevolve G2DListMutatorRealGaussian
+# Lucy mutator operator based on pyevolve G2DListMutatorRealGaussian modified 
+# using UnivariateSpline to smmoth the poses near the mutation point
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,7 +24,6 @@ from random import randint as rand_randint, gauss as rand_gauss, uniform as rand
 from random import choice as rand_choice
 import numpy as np
 from scipy.interpolate import UnivariateSpline
-#import matplotlib.pyplot as plt
 
 CDefG2DListMutRealMU = 0
 CDefG2DListMutRealSIGMA = 1
@@ -31,41 +31,41 @@ CDefG2DListMutRealSIGMA = 1
 CDefRangeMin = 0
 CDefRangeMax = 100
 
-interpolationPointsQty = 10
-num = interpolationPointsQty/2
 
 
-def interpolate(genome, which_x, which_y):
+def interpolate(genome, which_x, which_y, offset):
+
+   interpolationPointsQty = 10
+   which_y_InterpolationNeighborhood = interpolationPointsQty/2
+   minimunInterpolationNeighborhoodSize = 4
 
    if which_y - interpolationPointsQty/2 < 0:
-      interpolationPointsQty = interpolationPointsQty - abs(which_y - interpolationPointsQty/2) * 2
-      num = interpolationPointsQty/2
+      interpolationPointsQty = interpolationPointsQty - abs(which_y - which_y_InterpolationNeighborhood) * 2
+      which_y_InterpolationNeighborhood = interpolationPointsQty/2
 
    elif which_y + interpolationPointsQty/2 > genome.getHeight()-1:
-      interpolationPointsQty = interpolationPointsQty - (which_y + interpolationPointsQty/2 - (genome.getHeight()-1)) * 2
-      num = interpolationPointsQty/2
+      interpolationPointsQty = interpolationPointsQty - (which_y + which_y_InterpolationNeighborhood - (genome.getHeight()-1)) * 2
+      which_y_InterpolationNeighborhood = interpolationPointsQty/2
 
-   if interpolationPointsQty > 4:
+   if minimunInterpolationNeighborhoodSize > 4:
       x = np.ndarray(interpolationPointsQty)
       y = np.ndarray(interpolationPointsQty)
+      
       for k in xrange(interpolationPointsQty):
-         poseToSmooth = which_y - num + k
+         poseToSmooth = which_y - which_y_InterpolationNeighborhood + k
          x[k] = poseToSmooth
          y[k] = genome[poseToSmooth][which_x]
+      
       spl = UnivariateSpline(x, y)
-      spl.set_smoothing_factor(0.3) #2 - 0,3
-      #plt.plot(x, y, 'ro', ms=5)
-      #plt.plot(x, spl(x), 'g', lw=3)
+      spl.set_smoothing_factor(0.3)
 
       for k in xrange(interpolationPointsQty):
-         #print "xk", x[k], "intxk ", int(x[k])
-         #print "antes ", genome[int(x[k])][which_x], "ahora ", spl(int(x[k])), "k ", k, "diff ", genome[int(x[k])][which_x]-spl(int(x[k])), "offset ", offset
+         #print "before ", genome[int(x[k])][which_x], "now ", spl(int(x[k])), "k ", k, "diff ", genome[int(x[k])][which_x]-spl(int(x[k])), "offset ", offset
          genome.setItem(int(x[k]), which_x, spl(int(x[k])))   
 
 
-
 def G2DListMutatorRealGaussianSpline(genome, **args):
-   """ A gaussian mutator for G2DList of Real 
+   """ A gaussian mutator for G2DList of Real with UnivariateSpline for smoothing 
 
    Accepts the *rangemin* and *rangemax* genome parameters, both optional. Also
    accepts the parameter *gauss_mu* and the *gauss_sigma* which respectively
@@ -93,13 +93,14 @@ def G2DListMutatorRealGaussianSpline(genome, **args):
       for i in xrange(genome.getHeight()):
          for j in xrange(genome.getWidth()):
             if Util.randomFlipCoin(args["pmut"]):
-               final_value = genome[i][j] + rand_gauss(mu, sigma)
+               offset = rand_gauss(mu, sigma)
+               final_value = genome[i][j] + offset
 
-               final_value = min(final_value, genome.getParam("rangemax", Consts.CDefRangeMax))
-               final_value = max(final_value, genome.getParam("rangemin", Consts.CDefRangeMin))
+               final_value = min(final_value, genome.getParam("rangemax", CDefRangeMax))
+               final_value = max(final_value, genome.getParam("rangemin", CDefRangeMin))
 
-               genome.setItem(i, j, final_value)
-               
+               genome.setItem(i, j, final_value, offset)
+               interpolate(genome, j, i)
 
                mutations += 1
    else: 
@@ -107,40 +108,16 @@ def G2DListMutatorRealGaussianSpline(genome, **args):
       for it in xrange(int(round(mutations))):
          which_x = rand_randint(0, genome.getWidth()-1)  #joint to mutate
          which_y = rand_randint(0, genome.getHeight()-1) #pose to mutate
-         #print "which_x ", which_x
-         #print "which_y ", which_y
          offset = rand_gauss(mu, sigma)
          final_value = genome[which_y][which_x] + offset
 
+         #to be sure that the value is less than the rangemax and more than the rangemin value
          final_value = min(final_value, genome.getParam("rangemax", CDefRangeMax))
          final_value = max(final_value, genome.getParam("rangemin", CDefRangeMin))
 
          #valueBeforeMutation = genome[which_y][which_x]
          genome.setItem(which_y, which_x, final_value)
+         interpolate(genome, which_x, which_y, offset)
 
-         if which_y - interpolationPointsQty/2 < 0:
-            interpolationPointsQty = interpolationPointsQty - abs(which_y - interpolationPointsQty/2) * 2
-            num = interpolationPointsQty/2
-
-         elif which_y + interpolationPointsQty/2 > genome.getHeight()-1:
-            interpolationPointsQty = interpolationPointsQty - (which_y + interpolationPointsQty/2 - (genome.getHeight()-1)) * 2
-            num = interpolationPointsQty/2
-
-         if interpolationPointsQty > 4:
-            x = np.ndarray(interpolationPointsQty)
-            y = np.ndarray(interpolationPointsQty)
-            for k in xrange(interpolationPointsQty):
-               poseToSmooth = which_y - num + k
-               x[k] = poseToSmooth
-               y[k] = genome[poseToSmooth][which_x]
-            spl = UnivariateSpline(x, y)
-            spl.set_smoothing_factor(0.3) #2 - 0,3
-            #plt.plot(x, y, 'ro', ms=5)
-            #plt.plot(x, spl(x), 'g', lw=3)
-
-            for k in xrange(interpolationPointsQty):
-               #print "xk", x[k], "intxk ", int(x[k])
-               #print "antes ", genome[int(x[k])][which_x], "ahora ", spl(int(x[k])), "k ", k, "diff ", genome[int(x[k])][which_x]-spl(int(x[k])), "offset ", offset
-               genome.setItem(int(x[k]), which_x, spl(int(x[k])))   
 
    return int(mutations)
