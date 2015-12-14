68e2dae7ff132014cd78f63ef976f43b96e6c1c1
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2015-12-13 23:47:24 -0300

    avoid joints in DTIndividualPropertyVanillaEvolutiveare the ones used in the evolutive algorithm

--- datatypes/DTIndividualProperty.py
+++ datatypes/DTIndividualProperty.py
@@ -151,7 +151,7 @@ class DTIndividualPropertyVanillaEvolutive(DTIndividualProperty):
 
     def __init__(self):
         DTIndividualProperty.__init__(self)
-        self.avoidJoints = ["L_Elbow_Yaw", "R_Elbow_Yaw", "L_Shoulder_Yaw", "R_Shoulder_Yaw", "R_Hip_Yaw", "L_Hip_Yaw", "L_Ankle_Roll", "R_Ankle_Roll", "R_Shoulder_Pitch", "L_Shoulder_Pitch"]    
+        self.avoidJoints = ["L_Elbow_Yaw", "R_Elbow_Yaw", "L_Shoulder_Yaw", "R_Shoulder_Yaw", "R_Hip_Yaw", "L_Hip_Yaw", "L_Ankle_Roll", "R_Ankle_Roll", "L_Hip_Roll", "R_Hip_Roll"]    
         
         self.poseFix["R_Shoulder_Yaw"] = 0
         self.poseFix["R_Shoulder_Pitch"] = 0
