2c581635cfee13c099b73c84be575e1e3149a139
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2016-01-18 21:29:57 -0300

    refactoring

--- tests/testLucyEvolution.py
+++ tests/testLucyEvolution.py
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from simulator.SimLucy                          import SimLucy
+from simulator.Lucy                             import SimulatedLucy
 from simulator.AXAngle                          import AXAngle
 from parser.LoadPoses                           import LoadPoses
 from datatypes.DTIndividualProperty             import DTIndividualProperty, DTIndividualPropertyCMUDaz, DTIndividualPropertyVanilla, DTIndividualPropertyBaliero, DTIndividualPropertyVanillaEvolutive, DTIndividualPropertyPhysicalBioloid
