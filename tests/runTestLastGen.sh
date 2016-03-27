29be5b2ffb6bc7004170f66cd548843edb571c1c
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2016-03-27 12:30:51 -0300

    helper utility to see the GA results

---
+++ tests/runTestLastGen.sh
@@ -0,0 +1,14 @@
+readonly EXPERIMENT_DIRECTORY=20160327-021909
+ 
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-0*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-1*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-2*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-3*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-4*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-5*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-6*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-7*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-8*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-9*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-10*
+ ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-11*
