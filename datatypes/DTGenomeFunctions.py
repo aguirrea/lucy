59591cfab42d99717e425d884652a9e7570768ef
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2016-07-29 16:12:43 -0300

    adding interpolation of poses for the corssover instead of smoothing existing ones

--- datatypes/DTGenomeFunctions.py
+++ datatypes/DTGenomeFunctions.py
@@ -29,8 +29,7 @@ from datatypes.DTIndividualProperty import DTIndividualPropertyVanillaEvolutive
 from simulator.LoadRobotConfiguration import LoadRobotConfiguration
 
 INFINITE_DISTANCE = sys.maxint
-#SPLINE_SMOOTHING_FACTOR = 0.3
-SPLINE_SMOOTHING_FACTOR = 0.5
+SPLINE_SMOOTHING_FACTOR = 5
 SMOOTHING_WINDOW = 20
 
 class DTGenomeFunctions(object):
@@ -46,11 +45,12 @@ class DTGenomeFunctions(object):
         jointIndex = 0
         for joint in robotJoints:
             if not prop.avoidJoint(joint):
-                if frame1[jointIndex] == sysConstants.JOINT_SENTINEL or frame2[jointIndex] == sysConstants.JOINT_SENTINEL:
+                if frame1[jointIndex] == sysConstants.JOINT_SENTINEL or frame2[
+                    jointIndex] == sysConstants.JOINT_SENTINEL:
                     diff = INFINITE_DISTANCE
                     break
                 else:
-                    jointDiff = (frame1[jointIndex] - frame2[jointIndex])**2
+                    jointDiff = (frame1[jointIndex] - frame2[jointIndex]) ** 2
                     diff += jointDiff
             jointIndex = jointIndex + 1
         return diff
@@ -78,7 +78,7 @@ class DTGenomeFunctions(object):
     # returns the length of a genome, as the number of poses it contains until the JOINT_SENTINEL value is present in all the joints of a pose
     def getIndividualLength(self, genome):
         iter = 0
-        genomaHeight =  genome.getHeight()
+        genomaHeight = genome.getHeight()
         while iter < genomaHeight and not self.equalSentinelFrame(genome[iter]):
             iter = iter + 1
         length = iter
@@ -86,6 +86,42 @@ class DTGenomeFunctions(object):
 
     def getIndividualFrameLength(self, genome):
         return genome.getWidth()
+
+    def smooth(self, genome, which_x, which_y):
+        interpolationPointsQty = SMOOTHING_WINDOW
+        which_y_InterpolationNeighborhood = interpolationPointsQty / 2
+        minimunInterpolationNeighborhoodSize = 4
+
+        if which_y - interpolationPointsQty / 2 < 0:
+            interpolationPointsQty -= abs(which_y - which_y_InterpolationNeighborhood) * 2
+            which_y_InterpolationNeighborhood = interpolationPointsQty / 2
+
+        elif which_y + interpolationPointsQty / 2 > genome.getHeight() - 1:
+            interpolationPointsQty -= (which_y + which_y_InterpolationNeighborhood - (genome.getHeight() - 1)) * 2
+            which_y_InterpolationNeighborhood = interpolationPointsQty / 2
+
+        if which_y_InterpolationNeighborhood > minimunInterpolationNeighborhoodSize:
+            x = np.ndarray(interpolationPointsQty)
+            y = np.ndarray(interpolationPointsQty)
+
+            for k in xrange(interpolationPointsQty):
+                poseToSmooth = which_y - which_y_InterpolationNeighborhood + k
+                x[k] = poseToSmooth
+                y[k] = genome[poseToSmooth][which_x]
+
+            spl = UnivariateSpline(x, y, s=SPLINE_SMOOTHING_FACTOR)
+
+            for k in xrange(interpolationPointsQty):
+                # print "before ", genome[int(x[k])][which_x], "now ", spl(int(x[k])), "k ", k, "diff ", genome[int(x[k])][which_x]-spl(int(x[k])), "offset ", offset
+                # if x[k] == which_x:
+                #    print "no mutation"
+                # elif y[k]!= sysConstants.JOINT_SENTINEL:
+                if y[k] != sysConstants.JOINT_SENTINEL:
+                    newValue = spl(int(x[k]))
+                    genome.setItem(int(x[k]), which_x, newValue)
+                    ##print "mutating value: ", y[k], "to value: ", newValue, "difference: ", abs(y[k]-newValue)
+                    ##else:
+                    ##print "oh no!, we have a problem in mutation operator .. :S"
 
     def interpolate(self, genome, which_x, which_y):
         interpolationPointsQty = SMOOTHING_WINDOW
@@ -100,28 +136,24 @@ class DTGenomeFunctions(object):
             interpolationPointsQty -= (which_y + which_y_InterpolationNeighborhood - (genome.getHeight() - 1)) * 2
             which_y_InterpolationNeighborhood = interpolationPointsQty / 2
 
-        if  which_y_InterpolationNeighborhood > minimunInterpolationNeighborhoodSize:
-            x = np.ndarray(interpolationPointsQty)
-            y = np.ndarray(interpolationPointsQty)
+        interpolationWindow = interpolationPointsQty / 5
 
+        if which_y_InterpolationNeighborhood > minimunInterpolationNeighborhoodSize:
+            x = np.ndarray(interpolationPointsQty - interpolationWindow * 2)
+            y = np.ndarray(interpolationPointsQty - interpolationWindow * 2)
+
+            splineIndexCounter = 0
             for k in xrange(interpolationPointsQty):
                 poseToSmooth = which_y - which_y_InterpolationNeighborhood + k
-                x[k] = poseToSmooth
-                y[k] = genome[poseToSmooth][which_x]
+                if poseToSmooth <= which_y - interpolationWindow or poseToSmooth > which_y + interpolationWindow:
+                    x[splineIndexCounter] = poseToSmooth
+                    y[splineIndexCounter] = genome[poseToSmooth][which_x]
+                    splineIndexCounter += 1
 
-            spl = UnivariateSpline(x, y)
-            spl.set_smoothing_factor(SPLINE_SMOOTHING_FACTOR)
+            spl = UnivariateSpline(x, y, s=SPLINE_SMOOTHING_FACTOR)
 
             for k in xrange(interpolationPointsQty):
-                # print "before ", genome[int(x[k])][which_x], "now ", spl(int(x[k])), "k ", k, "diff ", genome[int(x[k])][which_x]-spl(int(x[k])), "offset ", offset
-                #if x[k] == which_x:
-                #    print "no mutation"
-                #elif y[k]!= sysConstants.JOINT_SENTINEL:
-                if y[k]!= sysConstants.JOINT_SENTINEL:
-                    newValue = spl(int(x[k]))
-                    genome.setItem(int(x[k]), which_x, newValue)
-                    ##print "mutating value: ", y[k], "to value: ", newValue, "difference: ", abs(y[k]-newValue)
-        ##else:
-            ##print "oh no!, we have a problem in mutation operator .. :S"
-
-
+                iter = which_y - which_y_InterpolationNeighborhood + k
+                if genome[iter][which_x] != sysConstants.JOINT_SENTINEL:
+                    newValue = spl(iter)
+                    genome.setItem(iter, which_x, newValue)
