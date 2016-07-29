4cb4e6a8bb377627e2b1f7bd1f4edd7eea420461
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2016-07-29 16:19:16 -0300

    using all the hip joints in evolution

--- datatypes/DTIndividualProperty.py
+++ datatypes/DTIndividualProperty.py
@@ -157,7 +157,7 @@ class DTIndividualPropertyVanillaEvolutive(DTIndividualProperty):
 
     def __init__(self):
         DTIndividualProperty.__init__(self)
-        self.avoidJoints = ["L_Elbow_Yaw", "R_Elbow_Yaw", "L_Shoulder_Yaw", "R_Shoulder_Yaw", "R_Hip_Yaw", "L_Hip_Yaw", "L_Ankle_Roll", "R_Ankle_Roll", "L_Hip_Roll", "R_Hip_Roll"]
+        self.avoidJoints = ["L_Elbow_Yaw", "R_Elbow_Yaw", "L_Shoulder_Yaw", "R_Shoulder_Yaw", "L_Ankle_Roll", "R_Ankle_Roll"]
         
         self.poseFix["R_Shoulder_Yaw"] = 0
         self.poseFix["R_Shoulder_Pitch"] = 0
