d2aac7a140f79b7b7365b2a60b687c661e14f9e9
Andrés Aguirre Dorelo <aaguirre@fing.edu.uy>
2016-12-26 16:14:31 -0300

    minor changes

--- tests/runTestLastGen.sh
+++ tests/runTestLastGen.sh
@@ -1,37 +1,37 @@
-readonly EXPERIMENT_DIRECTORY=20160820-233408
+EXPERIMENT_DIRECTORY=20160820-233408
  
 ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-0-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-1-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-2-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-3-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-4-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-5-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-6-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-7-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-8-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-9-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-10-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-11-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-1-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-2-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-3-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-4-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-5-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-6-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-7-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-8-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-9-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-10-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-11-*
 
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-12-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-13-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-14-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-15-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-16-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-17-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-18-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-19-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-20-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-21-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-12-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-13-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-14-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-15-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-16-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-17-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-18-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-19-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-20-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-21-*
 
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-22-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-23-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-24-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-25-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-26-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-27-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-28-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-29-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-30-*
- ./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-31-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-22-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-23-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-24-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-25-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-26-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-27-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-28-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-29-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-30-*
+./testLucyEvolution.py ../gene_pool/$EXPERIMENT_DIRECTORY/final-31-*
 
